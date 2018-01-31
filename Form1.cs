a0b11f21ef0db99ef86f4bd1e655425ad938a2ad
pkandroidnew <yong.dung.li@gmail.com>
2018-02-01 00:27:00 +0800

    mouse

--- Form1.cs
+++ Form1.cs
@@ -72,8 +72,20 @@ namespace AutoKeyboard
             SetForegroundWindow(calculatorHandle);
             SendKeys.SendWait("111");
             SendKeys.SendWait("*");
-            SendKeys.SendWait("12");
+            SendKeys.SendWait("13");
             SendKeys.SendWait("=");
         }
+
+        /// <summary>
+        /// 模擬壓住滑鼠右鍵。
+        /// </summary>
+        public static void RightDown()
+        {
+            NativeMethods.mouse_event(NativeContansts.MEF_RIGHTDOWN, 0, 0, 0, IntPtr.Zero);
+        }
+
+
+        public extern static void mouse_event(int dwFlags, int dx, int dy, int dwData, IntPtr dwExtraInfo);
+
     }
 }
